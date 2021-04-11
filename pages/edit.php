3c750f0d14bf3e400313e7af924369adc6dfd051
Yasmine091 <7486279-Yasmine091@users.noreply.gitlab.com>
2021-04-11 22:02:56 +0200

    Formulaire de modification d'entrées de journal

--- pages/edit.php
+++ pages/edit.php
@@ -1,40 +1,126 @@
 <?php
 require './core/config.php';
 
-$uid = $_GET['id'];
-$uid = mysqli_real_escape_string($con, $uid);
+if ($logged == '') {
+    header('Refresh: 0; URL=/');
+    exit();
+}
 
-$usql = "SELECT * FROM users WHERE id = '$uid' OR user ='$uid'";
-$ures = mysqli_query($con, $usql);
-while ($usrd = mysqli_fetch_assoc($ures)) {
+$dayID = (int) $_GET['day'];
 
-    $raid = $usrd['urank'];
-    $rasql = "SELECT * FROM ranks WHERE num = '$raid'";
-    $rres = mysqli_query($con, $rasql);
-    $rand = mysqli_fetch_assoc($rres);
+if (isset($_POST['edit-d'])) {
 
-?>
-    <div class="card bg-dark text-white border-none mb-3">
-        <div id="box-title" class="card-header text-<?php echo $wbcnf['text-var'] ?>">Profil de <?php echo $usrd['user'] ?></div>
-        <div class="card-body">
-            <img src="<?php echo $usrd['banner'] ?>" class="card-img">
-            <hr>
-            <div class="d-flex">
-                <img src="<?php echo $usrd['picture'] ?>" class="card-img" style="">
-                <p class="card-text">
-                    <span class="form-control" readonly>Pseudo: <?php echo $usrd['user'] ?></span>
-                    <span class="form-control" readonly>Rang:
-                        <span class="badge text-<?php echo $rand['text-var'] ?>" style="background-color: <?php echo $rand['bg-color'] ?>; font-size: smaller;"><?php echo $rand['name'] ?></span>
-                    </span>
-                    <span class="form-control border-dark" readonly>Humeur: <?php echo $usrd['motto'] ?></span>
-                    <span class="form-control border-dark" readonly>Création: <?php echo $usrd['regdat'] ?></span>
-                </p>
-            </div>
-            <hr>
-            <p class="card-text">
-                <textarea class="form-control border-dark" id="mybio" readonly><?php echo $usrd['mybio'] ?></textarea>
-            </p>
-        </div>
-    <?php
+    //$ = mysqli_real_escape_string($con, $_POST['']);
+    $feelings = mysqli_real_escape_string($con, $_POST['feelings']);
+    $mission = mysqli_real_escape_string($con, $_POST['mission']);
+    $tech = mysqli_real_escape_string($con, $_POST['tech']);
+    $next = mysqli_real_escape_string($con, $_POST['next']);
+    $tech = mysqli_real_escape_string($con, $_POST['tech']);
+    $todo = mysqli_real_escape_string($con, $_POST['todo']);
+    $done = mysqli_real_escape_string($con, $_POST['done']);
+    $problems = mysqli_real_escape_string($con, $_POST['problems']);
+    $solutions = mysqli_real_escape_string($con, $_POST['solutions']);
+    $resources = mysqli_real_escape_string($con, $_POST['resources']);
+
+    if (empty($_POST['feelings']) or empty($_POST['mission']) or empty($_POST['tech']) or empty($_POST['next']) or empty($_POST['todo']) or empty($_POST['done']) or empty($_POST['problems']) or empty($_POST['solutions']) or empty($_POST['resources'])) {
+        alert_danger('Veuillez completer tous les champs');
+    } else {
+
+        $sql = "UPDATE `records` 
+        SET 
+        `feelings` = '$feelings', 
+        `mission` = '$mission', 
+        `tech` = '$tech', 
+        `todo` = '$todo', 
+        `done` = '$done', 
+        `problems` = '$problems', 
+        `solutions` = '$solutions', 
+        `resources` = '$resources'
+        WHERE id = '$dayID'";
+        var_dump(mysqli_query($con, $sql));
+
+        alert_success('Entrée éditée avec succès');
+
+        header('Refresh: 2; URL=/');
     }
+}
+
+
+$Rsql = "SELECT * FROM records
+        WHERE id = '$dayID'";
+
+$Rres = mysqli_query($con, $Rsql);
+
+while ($recs = mysqli_fetch_assoc($Rres)) {
+
+// Se etiquetan los elementos del formulario y se encripta la contraseña
 ?>
+<div class="mb-5 pb-5">
+    <div class="col-sm-12 col-md-6 col-lg-4 my-3 mx-auto d-block">
+        <div class="card border-warning">
+            <h5 class="card-header text-center text-uppercase font-weight-bold text-light bg-warning">Éditer une entrée de journal</h5>
+            <div class="card-body">
+
+                <form method="POST" class="form-group">
+
+                    <p>
+                        <label for="feel-inp">Météo du jour</label>
+                        <textarea class="form-control border-warning" id="feel-inp" rows="3" name="feelings" placeholder="Je me sens..."><?= $recs['feelings'] ?></textarea>
+                    </p>
+                    <p>
+                        <label for="mission-inp">Mission du jour</label>
+                        <textarea class="form-control border-warning" id="mission-inp" rows="3" name="mission" placeholder="Compétence à mettre en
+œuvre : Réaliser une interface
+utilisateur, maquetter une
+appli..."><?= $recs['mission'] ?></textarea>
+                    </p>
+                    <p>
+                        <label for="tech-inp">Méthodes et technologies</label>
+                        <textarea class="form-control border-warning" id="tech-inp" rows="3" name="tech" placeholder="Moyens mis en œuvre :
+agilité, langages, réunion de
+cadrage-projet, coordination,
+validation..."><?= $recs['tech'] ?></textarea>
+                    </p>
+                    <p>
+                        <label for="next-inp">Porsuites à donner</label>
+                        <textarea class="form-control border-warning" id="next-inp" rows="3" name="next" placeholder="Feed-back, réorientation, Go
+vs No-GO, présentation,
+déploiement, développement,
+veille..."><?= $recs['next'] ?></textarea>
+                    </p>
+                    <p>
+                        <label for="todo-inp">Ce que j'avais à faire</label>
+                        <textarea class="form-control border-warning" id="todo-inp" rows="3" name="todo"><?= $recs['todo'] ?></textarea>
+                    </p>
+                    <p>
+                        <label for="done-inp">Ce que j'ai fait</label>
+                        <textarea class="form-control border-warning" id="done-inp" rows="3" name="done"><?= $recs['done'] ?></textarea>
+                    </p>
+                    <p>
+                        <label for="prob-inp">Problèmes rencontrés</label>
+                        <textarea class="form-control border-warning" id="prob-inp" rows="3" name="problems" placeholder="Problème technique, gestion
+temps, absence, cadrage,
+compréhension..."><?= $recs['problems'] ?></textarea>
+                    </p>
+                    <p>
+                        <label for="sol-inp">Solution et pourquoi le choix</label>
+                        <textarea class="form-control border-warning" id="sol-inp" rows="3" name="solutions" placeholder="Comment j’ai résolu les
+problèmes ? Pourquoi j’ai
+fait ces choix ?"><?= $recs['solutions'] ?></textarea>
+                    </p>
+                    <p>
+                        <label for="res-inp">Ressources utilisées</label>
+                        <textarea class="form-control border-warning" id="res-inp" rows="3" name="resources" placeholder="Ressources documentaires/humaines"><?= $recs['resources'] ?></textarea>
+                    </p>
+                    <p>
+                        <input type="submit" class="btn btn-warning border-warning w-100" value="Éditer" name="edit-d">
+                    </p>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+}
