171e45a7c9a0f16f1662e07a6d509c33c22672c6
Yasmine091 <7486279-Yasmine091@users.noreply.gitlab.com>
2021-04-11 21:36:37 +0200

    Script SQL

--- SQL/journal.sql
+++ SQL/journal.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le : sam. 10 avr. 2021 à 15:53
--- Version du serveur :  8.0.23-0ubuntu0.20.04.1
--- Version de PHP : 7.4.3
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 11-04-2021 a las 19:34:35
+-- Versión del servidor: 8.0.18
+-- Versión de PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,78 +19,53 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `journal`
+-- Base de datos: `journal`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `records`
+-- Estructura de tabla para la tabla `records`
 --
 
-CREATE TABLE `records` (
-  `id` int NOT NULL,
-  `month` int NOT NULL,
-  `week` int NOT NULL,
-  `day` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `feelings` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mission` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tech` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `next` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `todo` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `done` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `problems` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `solutions` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `resources` text COLLATE utf8mb4_unicode_ci NOT NULL
+DROP TABLE IF EXISTS `records`;
+CREATE TABLE IF NOT EXISTS `records` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `month` varchar(7) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `week` int(11) NOT NULL,
+  `day` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `feelings` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mission` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tech` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `next` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `todo` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `done` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `problems` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `solutions` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `resources` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Déchargement des données de la table `records`
---
-
-INSERT INTO `records` (`id`, `month`, `week`, `day`, `feelings`, `mission`, `tech`, `next`, `todo`, `done`, `problems`, `solutions`, `resources`) VALUES
-(1, 1, 1, 'Vendredi', '', '', '', '', '', '', '', '', '');
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Estructura de tabla para la tabla `users`
 --
 
-CREATE TABLE `users` (
-  `id` int NOT NULL,
-  `user` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ip` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `user` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `users`
+-- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `user`, `email`, `password`, `ip`) VALUES
-(1, 'Kuki', 'contact@crealivity.eu', '$2y$10$4WzhWYvU2A8JgEkdGj0eLel9SQnCnpIBsGA587LMaLSXrKQ/PEPFK', '176.180.86.109');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `records`
---
-ALTER TABLE `records`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `records`
---
-ALTER TABLE `records`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+INSERT INTO `users` (`id`, `user`, `email`, `password`) VALUES
+(1, 'Kuki', 'contact@crealivity.eu', '$2y$10$4WzhWYvU2A8JgEkdGj0eLel9SQnCnpIBsGA587LMaLSXrKQ/PEPFK');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
